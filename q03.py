17579ad0978123badd0301cf6f8b8ea3a1b6140f
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:05 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -2,4 +2,10 @@
 ## Imprima el maximo de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
-
+### Punto 3
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+TD_= pd.read_csv("tbl0.tsv", sep="\t")
+TD_3=tb.groupby('_c1').max()['_c2']
+print(TD_3)
