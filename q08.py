36914a29e489f4367196439f18f98c9869501fc8
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:14 -0500

    Update q08.py

--- q08.py
+++ q08.py
@@ -1,4 +1,10 @@
 ##
 ## Agregue el año como una columna a la tabla tbl0.tsv 
 ## 
-
+### Punto 8
+import pandas as pd
+import numpy as np
+TD_= pd.read_csv("tbl0.tsv", sep="\t")
+TD_8=TD_.copy()
+TD_8['anio'] = TD_8['_c3'].str.split('-').str[0]
+print(TD_8)
