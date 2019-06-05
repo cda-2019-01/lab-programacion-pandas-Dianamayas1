959446afd32a8e278fd1b8a44b198f7d5ec95f00
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:21 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -3,5 +3,10 @@
 ## y tbl2.tsv, compute la suma de tbl2._c5b por cada
 ## valor en tbl0._c1.
 ## 
-
+### Punto 12
+import pandas as pd
+import numpy as np
+TD_= pd.read_csv('tbl2.tsv', sep='\t')
+TD_12=TD_.groupby('_c5a')['_c5b'].sum()
+print(TD_12)
 
