b2389095b21b7b709deaf3f06de2adf769c91819
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:17 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,13 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
-
+## Punto 10
+import pandas as pd
+import numpy as np
+TD_ = pd.read_csv("tbl1.tsv", sep="\t")
+dfaux = TD_.groupby('_c0')['_c4'].apply(list)
+TD_10 = pd.DataFrame()
+TD_10 ['_c0'] = dfaux.keys()
+TD_10 ['lista'] = [elemt for elemt in dfaux]
+TD_10 ['lista'] = [",".join(str(v) for v in sorted(elemt)) for elemt in TD_10['lista']]
+print(TD_10)
