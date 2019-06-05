74858b36ee02c6c74136a23bad3cbaa75db7bfd6
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:01 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -2,3 +2,9 @@
 ## Imprima la cantidad de registros por cada letra 
 ## de la columna _c1 de la tabla tbl0
 ## 
+### Punto 1
+import pandas as pd
+import numpy as np
+TD_= pd.read_csv("tbl0.tsv", sep="\t")
+TD_1=TD_.groupby('_c1').count()['_c2']
+print(TD_1)
