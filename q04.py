9a29870fc356bdc3665a90d2745741e31e62322c
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:07 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -2,5 +2,13 @@
 ## Imprima los valores unicos e la columna _c4 de 
 ## de la tabla tbl1 en mayusculas
 ## 
-
+### Punto 4
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+TD_= pd.read_csv("tbl1.tsv", sep="\t")
+TD_4=TD_['_c4'].unique()
+TD_4= [line.upper() for line in TD_4]
+TD_4.sort()
+print(TD_4)
 
