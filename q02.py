21a764c4284fbf70e4e6a7f7f248c91d1e1567ee
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 02:41:04 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -2,4 +2,10 @@
 ## Imprima el promedio de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
-
+### Punto 2
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+TD_= pd.read_csv("tbl0.tsv", sep="\t")
+TD_2=tb.groupby('_c1')['_c2'].mean()
+print(TD_2)
